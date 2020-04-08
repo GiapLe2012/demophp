0f32a20144f6c6fbafd0ea63617a5f83577bb40a
Le Van Giap <giaple2012@gmail.com>
2020-04-08 14:50:58 +0700

    Update index.php
    d

--- myapp/index.php
+++ myapp/index.php
@@ -5,7 +5,7 @@
     </head>
     <body>
         <p>
-            Hello. Today is <?= date('l \t\h\e jS') ?>.
+            Hello GiapLV. Today is <?= date('l \t\h\e jS') ?>.
         </p>
     </body>
 </html>
